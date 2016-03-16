f49272dd5030e907d0b11f0a1e21f309e6c9d7e0
fanjiaqi <18611313085@163.com>
2016-03-16 21:57:15 +0800

    Signed-off-by: 模拟发送短信ok <18611313085@163.com>

--- send-msg/sendMsg.js
+++ send-msg/sendMsg.js
@@ -4,7 +4,9 @@ window.onload = function()
 	var oImg = document.getElementById('img1');
 	var oEnter = document.getElementById('enter');
 	var oSend = document.getElementById('send');
+	var oImgcontent = document.getElementById('imgcontent');
 	var onoff = true;
+	var str = oPanel.innerHTML;
 
 
 	oImg.onclick = function()
@@ -14,16 +16,27 @@ window.onload = function()
 		 {
 		 	oImg.src = 'img/2.png';
 		 	onoff = false;
+			oEnter.value = '';
 		 }else{
 
 		 	oImg.src = 'img/1.png';
 		 	onoff = true;
-		 };
+			oEnter.value = '';
+		 }
 	};
 
 	oSend.onclick = function()
 	{
-		oPanel.innerHTML = oEnter.value;
+		if(onoff == true)
+		{
+			str = '<div>' + oImgcontent.innerHTML + oEnter.value + '</div>' + str;
+			oPanel.innerHTML = str;
+		}
+		else{
+			str = '<div class="second"><div class="secondcontent">' + oEnter.value + oImgcontent.innerHTML + '</div></div>' + str;
+			oPanel.innerHTML = str;
+			
+		}
 	};
 
 
